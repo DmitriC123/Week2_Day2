89abc1aa81d2e55d832381c576eaa1ca0c5c1e06
DmitriC123 <cohan.dima@gmail.com>
2017-10-03 15:09:22 -0400

    loops practice

--- Week2_Day2/Program.cs
+++ Week2_Day2/Program.cs
@@ -11,36 +11,99 @@ namespace Week2_Day2
         static void Main(string[] args)
         {
             //Declare and initialize a string array of first names.Choose a name to find and print the IndexOf.
-            string[] firstNames = { "Cody", "Zack", "Dmitri", "Harold" };
-            Console.WriteLine(Array.LastIndexOf(firstNames, "Dmitri"));
+
+            //string[] firstNames = { "Cody", "Zack", "Dmitri", "Harold" };
+            //Console.WriteLine(Array.LastIndexOf(firstNames, "Dmitri"));
 
             //Create an array of lucky numbers. Each number should repeat at least once.Choose a lucky number, 
-                //find and print the last index of it.
-            int[] luckyNum = { 1, 4, 6, 4, 6, 7, 9, 1, 9, 7 };
-            Console.WriteLine(Array.LastIndexOf(luckyNum, 6));
+            //find and print the last index of it.
+
+            //int[] luckyNum = { 1, 4, 6, 4, 6, 7, 9, 1, 9, 7 };
+            //Console.WriteLine(Array.LastIndexOf(luckyNum, 6));
 
             //Create an array of alphabet characters. Print the first index of the array.
-                //Now reverse the array and print the first index.
-            string[] letters = { "a", "z", "b", "e", "f", "h" };
-            Console.WriteLine(letters[0]);
-            Array.Reverse(letters);
-            Console.WriteLine(letters[0]);
+            //Now reverse the array and print the first index.
+
+            //string[] letters = { "a", "z", "b", "e", "f", "h" };
+            //Console.WriteLine(letters[0]);
+            //Array.Reverse(letters);
+            //Console.WriteLine(letters[0]);
 
             //Create an array of student names in random order. Order the students’ names using the sort method.
-                    //Print the first and last students’ names.
-            string[] studentNames = { "Nathan", "Jeff", "Monica", "Chris", "Joey", "Steve" };
-            Array.Sort(studentNames);
-            Console.WriteLine(studentNames[0] + " " + studentNames[5]);
+            //Print the first and last students’ names.
+
+            //string[] studentNames = { "Nathan", "Jeff", "Monica", "Chris", "Joey", "Steve" };
+            //Array.Sort(studentNames);
+            //Console.WriteLine(studentNames[0] + " " + studentNames[5]);
 
             //Ask 10 people for their favorite numbers and create an array of those numbers.
-                //Order the numbers using the sort method.Print the first and last numbers.
-            int[] favNum = { 2, 4, 17, 8, 9, 13, 5, 16, 20, 18};
-            Array.Sort(favNum);
-            Console.WriteLine(favNum[0]);
-            Console.WriteLine(favNum[favNum.Length - 1]);
+            //Order the numbers using the sort method.Print the first and last numbers.
 
+            //int[] favNum = { 2, 4, 17, 8, 9, 13, 5, 16, 20, 18};
+            //Array.Sort(favNum);
+            //Console.WriteLine(favNum[0]);
+            //Console.WriteLine(favNum[favNum.Length - 1]);
+
+            //for(int i = 0; i <= 10; i+=2)
+            //{
+            //    //Console.WriteLine(i);
+            //}
+            //for(int counter = 50; counter <= 100; counter++)
+            //{
+            //    //Console.WriteLine(counter);
+            //}
+
+            //string greetings = "My name is Dmitri";
+            //string[] words = greetings.Split();
+            //for (int i = 0; i < words.Length; i++) 
+            //{
+            //    Console.WriteLine(words[i]);
+            //}
+
+            //Create an array called days with the elements Monday, Tuesday, and Wednesday. 
+            //Using a For Loop, print the elements of the days array.
+
+            //string[] days = { "Monday", "Tuesday", "Wednesday" };
+            //for(int i = 0; i < days.Length; i++)
+            //{
+            //    Console.WriteLine(days[i]);
+            //}
+
+            //Start with the string "Once upon a time" and create an array called storyWords using the Split method.
+            //Reverse the order of the elements in the array and using a For Loop, print each word.
+
+            //string fairyTale = "Once upon a time" ;
+            //string[] storyWords = fairyTale.Split();
+            //Array.Reverse(storyWords);
+            //for(int i = 0; i < storyWords.Length; i++)
+            //{
+            //    Console.WriteLine(storyWords[i]);
+            //}
+
+            //string[] months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+            //foreach (string month in months)
+            //{
+            //    Console.WriteLine(month);
+            //}
+
+            //Write a program that takes your favorite foods and prints them out one at a time. (Use a foreach loop.)
+
+            //string[] foods = { "hot dog", "cheeseburger", "fish", "pizza" };
+            //foreach (string food in foods)
+            //{
+            //    Console.WriteLine(food);
+            //}
+
+            //Create an array of lucky numbers(just any numbers you like). Using a foreach loop, print the following:\
+
+            //int[] luckyNums = { 1, 5, 7, 13, 8, 17 };
+            //foreach (int luck in luckyNums)
+            //{
+            //    Console.WriteLine("Your lucky number is: " + luck );
+            //}
 
 
         }
     }
 }
+
